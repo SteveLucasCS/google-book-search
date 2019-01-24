372c23f66103c771f76a7c9225261c5303367eda
stevelucascs <stevelucascs@gmail.com>
2019-01-23 23:07:33 -0600

    proxy added back in

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import Books from "./pages/Books";
 import Nav from "./components/Nav";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <Books />
-    </div>
+      <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Books} />
+          <Route component={Books} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
